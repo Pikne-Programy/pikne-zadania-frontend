32ceddc0adca89f20c581a7d7a30f1b23538e8a4
miloszwasacz <wasacz.dev@gmail.com>
2021-01-20 09:01:48 +0100

    Update path-manager.js

--- helper/tests/path-manager.js
+++ helper/tests/path-manager.js
@@ -1,7 +1,7 @@
 export function getBasePath() {
     if (window.location.href.includes('github.io')) {
         //const path = window.location.href.replace(/.+io(.+)\//, /$1/);
-        const path = window.location.href.replace(/(.+)\//, /$1/);
+        const path = window.location.href.replace(/(.+)\//, '$1');
         console.log(path);
         return path;
     } else
