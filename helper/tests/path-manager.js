b0fc1a242f778a24c27e7be5db01cca5c1cfce19
miloszwasacz <wasacz.dev@gmail.com>
2021-01-20 09:55:36 +0100

    Update path-manager.js

--- helper/tests/path-manager.js
+++ helper/tests/path-manager.js
@@ -1,8 +1,7 @@
 export function getBasePath() {
     //Fix for GH-Pages relative paths
-    if (window.location.href.includes('pikne-programy.github.io')) {
-        const path = window.location.href.replace(/(.+)\//, '$1');
-        return path;
-    } else
+    if (window.location.href.includes('pikne-programy.github.io'))
+        return window.location.href.replace(/(.+)\//, '$1');
+    else
         return '';
 }
