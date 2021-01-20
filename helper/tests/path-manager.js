face3091a8d4a28602fa5cbcad358cf47ead890f
miloszwasacz <wasacz.dev@gmail.com>
2021-01-20 08:49:40 +0100

    Make Path Manager use full url

--- helper/tests/path-manager.js
+++ helper/tests/path-manager.js
@@ -1,6 +1,9 @@
 export function getBasePath() {
-    if (window.location.href.includes('github.io'))
-        return '/pikne-programy-frontend';
-    else
+    if (window.location.href.includes('github.io')) {
+        //const path = window.location.href.replace(/.+io(.+)\//, /$1/);
+        const path = window.location.href.replace(/(.+)\//, /$1/);
+        console.log(path);
+        return path;
+    } else
         return '';
 }
