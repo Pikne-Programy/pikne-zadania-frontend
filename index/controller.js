76433ffd8c00bc8960faa7474dc700ac40c0b4fc
miloszwasacz <wasacz.dev@gmail.com>
2021-01-21 00:33:11 +0100

    Remove obsolete touch conditions

--- index/controller.js
+++ index/controller.js
@@ -135,9 +135,6 @@ function onMenuChanged(update = false) {
         ControllerUtils.toggleMenuLoading(false);
         $('#breadcrumbs').parent().show();
         $('#menu-list').show();
-        console.log(('ontouchstart' in window));
-        console.log((navigator.maxTouchPoints > 0));
-        console.log((navigator.msMaxTouchPoints > 0));
         console.log(Utils.isTouch());
         if (!Utils.isTouch()) {
             $('#menu-list-container').setCustomScrollbars({
